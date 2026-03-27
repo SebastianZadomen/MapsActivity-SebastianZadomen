4ad7552777f753c70e06763714e653f70b540f29
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2026-03-27 10:17:29 +0100

    Update AddMakerScreen and SupabaseClient.kt

--- app/src/main/java/actividad/mapsapp/Core/Permissions/AppPermission.kt
+++ app/src/main/java/actividad/mapsapp/Core/Permissions/AppPermission.kt
@@ -1,28 +1,11 @@
-
 package actividad.mapsapp.Core.Permissions
 
 import android.Manifest
-
 sealed class AppPermission(val permissions: List<String>) {
-
     object Location : AppPermission(
-        // Es buena práctica pedir el COARSE junto con el FINE en las versiones nuevas de Android
-        listOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        )
+        listOf(Manifest.permission.ACCESS_FINE_LOCATION)
     )
-
     object CameraAndAudio : AppPermission(
         listOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
-    )
-
-    // ¡NUEVO! Aquí creamos el permiso combinado que busca nuestra pantalla
-    object LocationAndCamera : AppPermission(
-        listOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.CAMERA
-        )
     )
 }
